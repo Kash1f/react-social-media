c38b33ed803f57af4c91c419b623571c6b0b485c
Kashif <ahmed.kashif1001@gmail.com>
2023-09-22 00:43:56 +0500

    moredivs

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,28 +1,34 @@
 import "./Topbar.css"
 import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-    <div className="topbarLeft">
-    <span className="logo">KashifSocial</span>
-    </div>
-    <div className="topbarCenter">
-    <div className="searchbar">
-      <SearchIcon/>
+      <div className="topbarLeft">
+        <span className="logo">KashifSocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon />
 
-      <input placeholder="Search for a friend, post or video" className="searchInput"/>
-    </div>
-    </div>
-
-    <div className="topbarRight">
-      <span className="topbarLink">Homepage</span>
-      <span className="topbarLink">Timeline</span>
+          <input placeholder="Search for a friend, post or video" className="searchInput" />
+        </div>
       </div>
 
-      <div className="topbarIcons"></div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
 
-</div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
