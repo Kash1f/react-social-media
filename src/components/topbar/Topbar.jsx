aa64dc329156c3641fa427f192f46b1c88416bc5
Kashif <ahmed.kashif1001@gmail.com>
2023-09-22 21:59:27 +0500

    topbarcss

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -30,17 +30,19 @@ export default function Topbar() {
         
           <div className="topbarIconItem">
             <Chat/>
-            <span className="topbarIconBadge">1</span>
+            <span className="topbarIconBadge">2</span>
           </div>
 
           <div className="topbarIconItem">
             <Notifications/>
             <span className="topbarIconBadge">1</span>
           </div>
-
-
-          
         </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+
+
+
+
       </div>
     </div>
   )
