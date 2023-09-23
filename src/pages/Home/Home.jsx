bbf330096034cd9ac69a8a4ff37d692f9c48a55e
Kashif <ahmed.kashif1001@gmail.com>
2023-09-23 15:38:19 +0500

    addingcomp

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Topbar from "../../components/topbar/Topbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+
+export default function Home() {
+  return (
+    <>
+  <Topbar/>
+  
+  <div className="homeContainer">
+  <Sidebar/>
+  <Feed/>
+  <Rightbar/>
+  </div>
+  </>
+  )
+}
