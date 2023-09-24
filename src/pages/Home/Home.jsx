11169ee8e5f68ad60eaebca270e9c954081b5ace
Kashif <ahmed.kashif1001@gmail.com>
2023-09-24 23:17:33 +0500

    formatting

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,8 @@
 import Topbar from "../../components/topbar/Topbar"
 import Sidebar from "../../components/sidebar/Sidebar"
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
+import "./Home.css"
 
 export default function Home() {
   return (
