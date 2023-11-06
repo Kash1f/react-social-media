651f5bc04fe0d24de6cac6455063eb29b42722dc
Kashif <ahmed.kashif1001@gmail.com>
2023-11-06 14:47:33 +0500

    first-commit

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,17 @@
-import "./Feed.css"
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./Feed.css";
+import { Posts } from "../../dummyData";
 
 export default function Feed() {
   return (
-    <div>Feed</div>
-  )
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,49 +1,42 @@
-import "./Topbar.css"
-import {Search, Person, Chat, Notifications} from '@mui/icons-material';
-
+import "./Topbar.css";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">KashifSocial</span>
+        <span className="logo">Lamasocial</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
-          <Search className="searchIcon"/>
-
-          <input placeholder="Search for a friend, post or video" className="searchInput" />
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
         </div>
       </div>
-
       <div className="topbarRight">
         <div className="topbarLinks">
           <span className="topbarLink">Homepage</span>
           <span className="topbarLink">Timeline</span>
         </div>
-
         <div className="topbarIcons">
           <div className="topbarIconItem">
-            <Person/>
+            <Person />
             <span className="topbarIconBadge">1</span>
           </div>
-        
           <div className="topbarIconItem">
-            <Chat/>
+            <Chat />
             <span className="topbarIconBadge">2</span>
           </div>
-
           <div className="topbarIconItem">
-            <Notifications/>
+            <Notifications />
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
-        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
-
-
-
-
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
       </div>
     </div>
-  )
+  );
 }
